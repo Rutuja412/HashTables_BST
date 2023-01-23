cb0808ebfef9c394e3356aa6f8303c32bc439bf1
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-23 11:05:28 +0530

    [Rutuja]:Create BST

---
+++ HashTable_BinarySearchTree/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HashTable_BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*****Binary Search Tree & HashTables******");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            
+            binarySearch.Display();
+        }
+    }
+}

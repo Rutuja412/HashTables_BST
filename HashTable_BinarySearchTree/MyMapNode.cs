8a24be8d61b8ff18f02e84caa953c4d63aafdbc2
Rutuja412 <rutujakadam4545@gmail.com>
2023-01-24 11:43:43 +0530

    [Rutuja]:Finf Frequency of word in Sentence

---
+++ HashTable_BinarySearchTree/MyMapNode.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashTable_BinarySearchTree
+{
+    class MyMapNode<K, V>//placeholder key,value string
+    {
+        private readonly int size;//global
+        private readonly LinkedList<KeyValue<K, V>>[] items;//global array type linked list
+        public MyMapNode(int size)//local//value is 5
+        {
+            this.size = size;//5 initiallize
+            this.items = new LinkedList<KeyValue<K, V>>[size]; //passing size 5//item pointing to array which is non primitive so it is null
+            //set up completed
+        }
+
+        public void Add(K key, V value)
+        {
+            int position = GetArrayPosition(key);  // |-5| =5 |3|=3 |-3|=3//methid
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            KeyValue<K, V> item = new KeyValue<K, V>() { Key = key, Value = value };//ex.key 0,value welcome
+
+            linkedList.AddLast(item);//if we have same position for 2 key values the this are added in linkedlist not in array
+        }
+
+        public void Remove(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);
+            bool itemFound = false;
+            KeyValue<K, V> foundItem = default(KeyValue<K, V>);
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    itemFound = true;
+                    foundItem = item;
+                }
+            }
+            if (itemFound)
+            {
+                linkedList.Remove(foundItem);
+            }
+        }
+
+        public V Get(K key)
+        {
+            int position = GetArrayPosition(key);
+            LinkedList<KeyValue<K, V>> linkedList = GetLinkedList(position);//passing same position
+            foreach (KeyValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+        protected int GetArrayPosition(K key)
+        {
+            var hashCode = key.GetHashCode();
+            int position = key.GetHashCode() % size;//5//return rem//GetHashCode is used to get hashcode //here we generate hashcode with particular key
+            return Math.Abs(position);//abs used for making no.positive
+
+
+        }
+
+       
+        public void Display()
+        {
+            
+            foreach (var linkedList in items)
+            {
+                if (linkedList != null)
+                    foreach (var element in linkedList)
+                    {
+                        string s = element.ToString();
+                        if (s != null)
+                            Console.WriteLine(element.Key + " " + element.Value);
+                    }
+            }
+        }
+
+
+        protected LinkedList<KeyValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KeyValue<K, V>> linkedList = items[position];// at positin 2 in array is null
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KeyValue<K, V>>();//one object is created
+                items[position] = linkedList;//store address stored in linkedlist Object in positin 
+            }
+            return linkedList;//returns the address of linkedlist
+            //linkedlist is local variable so after completion it will be deleted from stack
+        }
+    }
+
+    public struct KeyValue<k, v>    // struct is a value type
+    {
+
+        public k Key { get; set; }
+        public v Value { get; set; }
+    }
+
+}
--- HashTable_BinarySearchTree/Program.cs
+++ HashTable_BinarySearchTree/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace HashTable_BinarySearchTree
 {
@@ -7,26 +8,51 @@ namespace HashTable_BinarySearchTree
         static void Main(string[] args)
         {
             Console.WriteLine("*****Binary Search Tree & HashTables******");
-            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
-            binarySearch.Insert(30);
-            binarySearch.Insert(70);
-            binarySearch.Insert(22);
-            binarySearch.Insert(40);
-            binarySearch.Insert(60);
-            binarySearch.Insert(95);
-            binarySearch.Insert(11);
-            binarySearch.Insert(65);
-            binarySearch.Insert(3);
-            binarySearch.Insert(16);
-            binarySearch.Insert(63);
-            binarySearch.Insert(67);
-            binarySearch.Display();
-            Console.WriteLine("_______________________");
-            binarySearch.GetSize();
-            Console.WriteLine("__________________");
-            bool result = binarySearch.IfExists(67, binarySearch);
-            Console.WriteLine(result);
-            Console.ReadKey();
+            Console.WriteLine("Select given options");
+            Console.WriteLine("1.Binary Search Tree\n2.HashTble \n.Exit ");
+            int option = Convert.ToInt16(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+                    binarySearch.Insert(30);
+                    binarySearch.Insert(70);
+                    binarySearch.Insert(22);
+                    binarySearch.Insert(40);
+                    binarySearch.Insert(60);
+                    binarySearch.Insert(95);
+                    binarySearch.Insert(11);
+                    binarySearch.Insert(65);
+                    binarySearch.Insert(3);
+                    binarySearch.Insert(16);
+                    binarySearch.Insert(63);
+                    binarySearch.Insert(67);
+                    binarySearch.Display();
+                    Console.WriteLine("_______________________");
+                    binarySearch.GetSize();
+                    Console.WriteLine("__________________");
+                    bool result = binarySearch.IfExists(67, binarySearch);
+                    Console.WriteLine(result);
+                    Console.ReadKey();
+                    break;
+                    case 2:
+                    MyMapNode<string, string> hash = new MyMapNode<string, string>(5);
+
+                    hash.Add("0", "To");
+                    hash.Add("1", "be");
+                    hash.Add("2", "or");
+                    hash.Add("3", "not");
+                    hash.Add("4", "to");
+                    hash.Add("5", "be");
+                    hash.Display();
+                    string getword = hash.Get("3");
+                    Console.WriteLine("4th index value is : " + getword);
+                    break; 
+                default:
+                    Console.WriteLine("Select valid options only ");
+                    break;
+
+            }
         }
     }
 }
